9a8d0b57833071731bb3b68052757f0098a7e5fb
AF_rad <rosemary.adejoh@appsfactory.de>
2020-06-28 23:14:35 +0200

    test bestanden \(-_-)/

--- FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace FizzBuzz
+namespace FizzBuzzCode
+
 {
     public class FizzBuzz
     {
-        public static string[] ReturnString(int input)
+        public IEnumerable<string> ReturnString(int maxNumber)
         {
-            throw new NotImplementedException("Not yet implemented");
+            var result = new List<string>();
+
+            for (int i = 1; i <= maxNumber ; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    result.Add("fizz");
+                }
+                else if(i % 5 == 0) 
+                {
+                    result.Add("buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+
+            return result;
+            //throw new NotImplementedException();
         }
 
         static void Main()
--- FizzBuzz/FizzBuzz/FizzBuzzTests/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzTests/FizzBuzzTest.cs
@@ -1,18 +1,24 @@
-using System;
+using FizzBuzzCode;
+using System.Collections.Generic;
 using Xunit;
-using FizzBuzz;
 
 namespace FizzBuzzTests
 {
     public class FizzBuzzTest
     {
+        private readonly FizzBuzz _fizzBuzz;
+
+        public FizzBuzzTest()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
         [Fact]
         public void ReturnString_ShouldReturnIntegerAsString()
         {
             //Arrange
-            string[] expected = new string[] { "One" };
+            List<string> expected  = new List<string> { "1", "2", "fizz", "4", "buzz" };
             //Act
-            string[] actual = FizzBuzz.FizzBuzz.ReturnString(1);
+            List<string> actual = (List<string>)_fizzBuzz.ReturnString(5);
             //Assert
             Assert.Equal(expected, actual);
         }
